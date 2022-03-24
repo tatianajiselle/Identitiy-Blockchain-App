738af70cb9bf24d3d5f155e0a7d33f6dbef6be85
Tatiana Ensslin <tjexoxo@gmail.com>
2022-03-24 11:00:33 -0700

    Update index.js

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ app.use(bodyParser.json({ type: '*/*' }))
 
 const credentials = new Credentials({
   network: 'ropsten',
-  did: "did:ethr:0xc1050a01fadbbc82353d6787ba489622edfe63b6",
-  privateKey: "774d4c1cf796f7911c54ce1be9da2d743de4cd99f121999ecf4c29b733063a88"
+  did: process.env.DID,
+  privateKey: process.env.PRIVATE_KEY
 })
 
 /**
